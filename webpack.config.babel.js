6ddbb80594b5f3b4b40f0655a02df3f94ad5e9e7
Benjamin Huang <benjihuang818@gmail.com>
2016-06-12 18:23:40 +0800

    chore(webpack): Fill out the webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,11 +2,21 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 
-const htmlminOptions = {
+const IS_BUILD = (process.env.NODE_ENV === 'build')
 
+const JS_NAME = IS_BUILD ? 'js/[name].[chunkhash:8].js' : 'js/[name].js'
+const CSS_NAME = IS_BUILD ? 'css/[name].[chunkhash:8].css' : 'css/[name].css'
+const ASSETS_NAME = IS_BUILD ? 'assets/[name].[hash:8].[ext]' : 'assets/[name].[ext]'
+
+const htmlminOptions = {
+  collapseWhitespace: IS_BUILD,
+  removeComments: IS_BUILD,
+  removeEmptyAttributes: IS_BUILD,
+  sortAttributes: IS_BUILD,
+  sortClassName: IS_BUILD
 }
 
-export default {
+let config = {
   entry: {
     iconfonts: [
       './gh-pages/common.styl',
@@ -21,7 +31,7 @@ export default {
   },
   output: {
     path: './build',
-    filename: 'js/[name].[hash:8].js',
+    filename: JS_NAME,
     publicPath: 'http://localhost:8080/'
   },
   resolve: {
@@ -49,9 +59,13 @@ export default {
         loader: 'ejs-compiled'
       },
       {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
         test: /\.(gif|jpeg|jpg|png)(\?\S*)?$/,
         query: {
-          name: 'assets/[name].[hash:8].[ext]',
+          name: ASSETS_NAME,
           limit: 1024
         },
         loader: 'url'
@@ -59,7 +73,7 @@ export default {
       {
         test: /\.eot(\?\S*)?$/,
         query: {
-          name: 'assets/[name].[hash:8].[ext]',
+          name: ASSETS_NAME,
           limit: 1024
         },
         loader: 'url'
@@ -67,7 +81,7 @@ export default {
       {
         test: /\.svg(\?\S*)?$/,
         query: {
-          name: 'assets/[name].[hash:8].[ext]',
+          name: ASSETS_NAME,
           limit: 1024,
           minetype: 'image/svg+xml'
         },
@@ -76,7 +90,7 @@ export default {
       {
         test: /\.ttf(\?\S*)?$/,
         query: {
-          name: 'assets/[name].[hash:8].[ext]',
+          name: ASSETS_NAME,
           limit: 1024,
           minetype: 'application/octet-stream'
         },
@@ -85,7 +99,7 @@ export default {
       {
         test: /\.woff(\?\S*)?$/,
         query: {
-          name: 'assets/[name].[hash:8].[ext]',
+          name: ASSETS_NAME,
           limit: 1024,
           minetype: 'application/font-woff'
         },
@@ -94,7 +108,7 @@ export default {
       {
         test: /\.woff2(\?\S*)?$/,
         query: {
-          name: 'assets/[name].[hash:8].[ext]',
+          name: ASSETS_NAME,
           limit: 1024,
           minetype: 'application/font-woff2'
         },
@@ -103,26 +117,29 @@ export default {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('css/[name].[hash:8].css', {
-      disable: process.env.NODE_ENV !== 'BUILD'
+    new ExtractTextPlugin(CSS_NAME, {
+      disable: !IS_BUILD
     }),
     new HtmlWebpackPlugin({
       inject: false,
       minify: htmlminOptions,
       filename: 'index.html',
-      template: './gh-pages/index.html'
+      template: './gh-pages/index.html',
+      showErrors: true
     }),
     new HtmlWebpackPlugin({
       inject: false,
       minify: htmlminOptions,
       filename: 'iconfonts/index.html',
-      template: './gh-pages/iconfonts/index.html'
+      template: './gh-pages/iconfonts/index.html',
+      showErrors: true
     }),
     new HtmlWebpackPlugin({
       inject: false,
       minify: htmlminOptions,
       filename: 'svgs/index.html',
-      template: './gh-pages/svgs/index.html'
+      template: './gh-pages/svgs/index.html',
+      showErrors: true
     })
   ],
   postcss(bundle) {
@@ -130,10 +147,42 @@ export default {
       require('autoprefixer')
     ]
 
-    if (process.env.NODE_ENV === 'BUILD') {
+    if (IS_BUILD) {
       plugins.push(require('cssnano'))
     }
 
     return plugins
   }
 }
+
+if (!IS_BUILD) {
+  config.debug = true
+  config.devtool = 'eval-source-map'
+
+  config.plugins = config.plugins.concat([
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ])
+
+  Object.keys(config.entry).forEach(function(key) {
+    config.entry[key] = config.entry[key].concat(
+      'webpack/hot/dev-server',
+      `webpack-dev-server/client?http://localhost:8080`
+    )
+  })
+}
+
+if (IS_BUILD) {
+  config.plugins = config.plugins.concat(
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: false
+    })
+  )
+}
+
+export default config
